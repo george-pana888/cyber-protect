386a6f78a34035cf81e599b594ed24c21b789e91
george-pana888 <128566386+george-pana888@users.noreply.github.com>
2023-09-01 15:38:41 +1000

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the primary purpose of a firewall in cybersecurity?",
+    answer: "To prevent unauthorized access to a network or system",
+    options: [
+      "To detect and remove malware from the system",
+      "To encrypt data during transmission",
+      "To prevent unauthorized access to a network or system",
+      "To track and monitor user activities"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is a common method used to protect passwords from unauthorized access?",
+    answer: "Enabling multi-factor authentication (MFA)",
+    options: [
+      "Writing them down on a sticky note and keeping it on the desk",
+      "Using weak and easily guessable passwords",
+      "Enabling multi-factor authentication (MFA)",
+      "Sharing passwords with colleagues for convenience"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does the term phishing refer to in the context of cybersecurity?",
+    answer: "A social engineering attack that tricks users into revealing sensitive information",
+    options: [
+      "The process of encrypting sensitive data",
+      "A type of malware that spreads through email attachments",
+      "The act of attempting to access a system by guessing passwords",
+      "A social engineering attack that tricks users into revealing sensitive information"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which cybersecurity practice involves making an identical copy of data to protect against data loss?",
+    answer: "Data backup",
+    options: [
+      "Data sanitization",
+      "Data encryption",
+      "Data backup",
+      "Data breach"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does VPN stand for in the context of cybersecurity?",
+    answer: "Virtual Private Network",
+    options: [
+      "Virtual Private Network",
+      "Very Personal Network",
+      "Verified Public Network",
+      "Vital Protection Network"
+    ]
+  },
+];
